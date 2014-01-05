f3fdbb315f8cd46d23f4fb5162d50270abc9040b
petervilja <peter.vilja@gmail.com>
2014-01-05 15:27:30 +0200

    Avoid removing files and folders outside the current working directory. Files and folders outside the cwd can be removed with option force.

--- index.js
+++ index.js
@@ -4,25 +4,40 @@
 
 var map = require('map-stream'),
 	rimraf = require('rimraf'),
-	fs = require('fs');
+	fs = require('fs'),
+	gutil = require('gulp-util'),
+	path = require('path');
 
-module.exports = function(){
+module.exports = function(options){
 	return map(function (file, cb){
-		rimraf(file.path, function (err) {
-			if (!err || !fs.existsSync(file.path)) {
-				return cb(null, file);
-			}
-			rimraf(file.path, function (err) {
-				if (!err || !fs.existsSync(file.path)) {
+		var cwd = file.cwd || process.cwd();
+		// For safety resolve paths always resolve pathss
+		var filepath = path.resolve(cwd, file.path);
+		var starts = new RegExp('^' + cwd + '/');
+
+    	if (starts.test(filepath) && filepath !== cwd || (options ? options.force : false)) {	
+			rimraf(filepath, function (err) {
+				if (!err || !fs.existsSync(filepath)) {
 					return cb(null, file);
 				}
-				rimraf(file.path, function (err) {
-					if (!err || !fs.existsSync(file.path)) {
+				rimraf(filepath, function (err) {
+					if (!err || !fs.existsSync(filepath)) {
 						return cb(null, file);
 					}
-					return cb(err);
+					rimraf(filepath, function (err) {
+						if (!err || !fs.existsSync(filepath)) {
+							return cb(null, file);
+						}
+						return cb(err);
+					});
 				});
 			});
-		});
+		} else if (filepath === cwd) {
+			gutil.log('gulp-rimraf: Cannot delete the current working directory. (' + filepath + ')');
+      		cb(null, file);
+		} else {
+			gutil.log('gulp-rimraf: Cannot delete files or folders outside the current working directory. (' + filepath + ')');
+      		cb(null, file);
+		}
 	});
 };
